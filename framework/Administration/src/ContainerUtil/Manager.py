841b1769588c0329f81e126e123a09c6dc8427aa
Dominique Hunziker <dominique.hunziker@gmail.com>
2012-06-07 14:50:50 +0200

    update ContainerManager

--- framework/Administration/src/ContainerUtil/Manager.py
+++ framework/Administration/src/ContainerUtil/Manager.py
@@ -275,7 +275,7 @@ class ContainerManager(object):
                 log.msg(reason)
                 deferred.errback(reason.getErrorMessage())
             else:
-                deferred.callback()
+                deferred.callback(None)
         
         _deferred.addCallbacks(callback, callback)
         
@@ -308,7 +308,7 @@ class ContainerManager(object):
         
         self._reactor.callInThread(self._startContainer, deferred, commID, homeDir)
         
-        def reportSuccess():
+        def reportSuccess(_):
             log.msg('Container successfully started.')
         
         def reportFailure(msg):
@@ -370,7 +370,7 @@ class ContainerManager(object):
         
         self._reactor.callInThread(self._stopContainer, deferred, commID)
         
-        def reportSuccess():
+        def reportSuccess(_):
             log.msg('Container successfully stopped.')
         
         def reportFailure(msg):
