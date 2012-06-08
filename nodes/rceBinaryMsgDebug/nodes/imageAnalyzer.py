dba16b1711278d9358816d5be1630c688eef2343
Mohanarajah Gajamohan <gajamohan.m@gmail.com>
2012-06-08 17:27:11 +0200

    Adding a working copy of binary message debugging package

---
+++ nodes/rceBinaryMsgDebug/nodes/imageAnalyzer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import roslib
+import roslib; roslib.load_manifest('rceBinaryMsgDebug')
+
+import sys
+import rospy
+import cv
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic_results",Image)
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv(data, "bgr8")
+    except CvBridgeError, e:
+      print e
+
+    (cols,rows) = cv.GetSize(cv_image)
+    if cols > 60 and rows > 60 :
+      cv.Circle(cv_image, (50,50), 10, 255)
+
+    cv.ShowImage("Image window", cv_image)
+    cv.WaitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError, e:
+      print e
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print "Shutting down"
+  cv.DestroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
