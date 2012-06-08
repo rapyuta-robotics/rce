4a8605d7a62280b770ab01d8eb5bc8595df9ce19
Gajamohan Mohanarajah <gajamohan.m@gmail.com>
2012-06-08 09:29:33 +0200

    Fixed f**king indentation issues

--- clients/debugServer/echo_server.py
+++ clients/debugServer/echo_server.py
@@ -3,11 +3,11 @@ from twisted.internet import reactor
 from autobahn.websocket import WebSocketServerFactory, WebSocketServerProtocol, listenWS
 
 class CloudEngineProtocol(WebSocketServerProtocol):
-	def onMessage(self, msg, binary):
-		self.sendMessage(msg, binary)
+    def onMessage(self, msg, binary):
+        self.sendMessage(msg, binary)
 
 if __name__ == '__main__':
-	factory = WebSocketServerFactory("ws://localhost:9000")
-	factory.protocol = CloudEngineProtocol
-	listenWS(factory)
-	reactor.run()
+    factory = WebSocketServerFactory("ws://localhost:9000")
+    factory.protocol = CloudEngineProtocol
+    listenWS(factory)
+    reactor.run()
--- clients/python/echo_client.py
+++ clients/python/echo_client.py
@@ -1,22 +1,20 @@
+#!/usr/bin/env python
 from twisted.internet import reactor
 from autobahn.websocket import WebSocketClientFactory, WebSocketClientProtocol, connectWS
- 
- 
-class EchoClientProtocol(WebSocketClientProtocol):
- 
-	def sendHello(self):
-		self.sendMessage("Hello, world!")
- 
-	def onOpen(self):
-		self.sendHello()
- 
-	def onMessage(self, msg, binary):
-		print "Got echo: " + msg
-		reactor.callLater(1, self.sendHello)
+
+class rceClientProtocol(WebSocketClientProtocol):
+    def sendHello(self):
+        self.sendMessage("Hello, world!")
+
+    def onOpen(self):
+        self.sendHello()
+
+    def onMessage(self, msg, binary):
+        print "Got echo: " + msg
+        reactor.callLater(1, self.sendHello)
 
 if __name__ == '__main__':
- 
-	factory = WebSocketClientFactory("ws://localhost:9000")
-	factory.protocol = EchoClientProtocol
-	connectWS(factory)
-	reactor.run()
+    factory = WebSocketClientFactory("ws://localhost:9000")
+    factory.protocol = rceClientProtocol
+    connectWS(factory)
+    reactor.run()
