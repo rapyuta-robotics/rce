71afcee0b0d4b098b9e8bf68af380534be31cf32
Gajamohan Mohanarajah <gajamohan.m@gmail.com>
2012-06-13 11:45:42 +0200

    Added binary file handling for the html client

--- clients/debugConnection/binaryMsgTestServer.py
+++ clients/debugConnection/binaryMsgTestServer.py
@@ -6,7 +6,7 @@ import uuid
 class CloudEngineProtocol(WebSocketServerProtocol):
     def onMessage(self, msg, binary):
         if binary:
-            self.sendMessage('received binary message')
+            #self.sendMessage('received binary message')
             #f = open('apple_copy.png','w')
             #f.write(msg[33:])
             #f.close()
@@ -15,10 +15,13 @@ class CloudEngineProtocol(WebSocketServerProtocol):
             #self.sendMessage(msg[32:], binary=True)
             tmpUUID = uuid.uuid4().hex
             print 'UUID <- '+ tmpUUID
+            JSONImageMsg = '{"type":"CM","dest":"robotUniqueID","orig":"containerTag01","data":{"type":"sensor_msgs/Image","msgID":"msgID_0","interfaceTag":"Test/modifyImage","msg":{"data":'+tmpUUID+'}}}'
+            self.sendMessage(JSONImageMsg)
             self.sendMessage(tmpUUID+msg[32:], binary=True)
             #self.sendMessage(msg, binary=True)
         else:
-            self.sendMessage('received non binary message')
+            #self.sendMessage('received non binary message')
+            print msg
             
     def onOpen(self):
         print 'connection made.'
