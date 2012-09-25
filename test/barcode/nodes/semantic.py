c93e7fd6994619dba8a15b810c8ae7c3d4adaf50
Dominique Hunziker <dominique.hunziker@gmail.com>
2012-09-25 16:46:58 +0200

    modify license

--- test/barcode/nodes/semantic.py
+++ test/barcode/nodes/semantic.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#       TermFrequency.py
-#       
-#       Copyright 2011 dominique hunziker <dominique.hunziker@gmail.com>
-#       
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
-#       
-#       
+#     
+#     semantic.py
+#     
+#     This file is part of the RoboEarth Cloud Engine test.
+#     
+#     This file was originally created for RoboEearth
+#     http://www.roboearth.org/
+#     
+#     The research leading to these results has received funding from
+#     the European Union Seventh Framework Programme FP7/2007-2013 under
+#     grant agreement no248942 RoboEarth.
+#     
+#     Copyright 2012 RoboEarth
+#     
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#     
+#     http://www.apache.org/licenses/LICENSE-2.0
+#     
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#     
+#     \author/s: Dominique Hunziker
+#     
+#     
 
 import itertools
 from string import lower
