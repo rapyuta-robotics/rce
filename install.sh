71b83220047ae53840513613564299b1f1abae88
Dominique Hunziker <dominique.hunziker@gmail.com>
2013-03-28 16:00:26 +0100

    add install script; update .gitignore

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+install_pkg() {
+    title="Install package $1"
+    
+    printf "%s\n" "$title"
+    for i in $(seq 1 ${#title})
+    do 
+        printf "-"
+    done
+    printf "\n\n"
+    
+    cd $1
+    rm -rf build dist *.egg-info
+    python setup.py install
+    cd ..
+    
+    printf "\n\n"
+}
+
+install_pkg rce-util
+install_pkg rce-comm
+install_pkg rce-core
+install_pkg rce-client
+install_pkg rce-console
