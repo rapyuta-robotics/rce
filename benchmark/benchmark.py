a12aedb1182b09f891b474163d5d53de901799ef
Dominique Hunziker <dominique.hunziker@gmail.com>
2013-04-14 16:40:30 +0200

    fix benchmark to work with new Interface implementation

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#     
+#
 #     benchmark.py
-#     
+#
 #     This file is part of the RoboEarth Cloud Engine framework.
-#     
+#
 #     This file was originally created for RoboEearth
 #     http://www.roboearth.org/
-#     
+#
 #     The research leading to these results has received funding from
 #     the European Union Seventh Framework Programme FP7/2007-2013 under
 #     grant agreement no248942 RoboEarth.
-#     
+#
 #     Copyright 2012 RoboEarth
-#     
+#
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
 #     You may obtain a copy of the License at
-#     
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#     
+#
 #     Unless required by applicable law or agreed to in writing, software
 #     distributed under the License is distributed on an "AS IS" BASIS,
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
-#     
-#     \author/s: Dominique Hunziker 
-#     
-#     
+#
+#     \author/s: Dominique Hunziker
+#
+#
 
 # Python specific imports
 import time
@@ -49,31 +49,31 @@ class TestBase(object):
         self._name = name
         self._style = style
         self._color = color
-        
+
         self._deferred = []
         self._data = []
-    
+
     def registerDeferred(self, d):
         self._deferred.append(d)
-    
+
     def _activate(self):
         raise NotImplementedError('Test has to implement this method.')
-    
+
     def _deactivate(self):
         raise NotImplementedError('Test has to implement this method.')
-    
+
     def _run(self):
         raise NotImplementedError('Test has to implement this method.')
-    
+
     def run(self, _):
         print('Run test: {0}'.format(self._name))
         self._conn.reactor.callLater(1, self._activate)
         self._conn.reactor.callLater(2, self._run)
-    
+
     def _done(self):
         self._deactivate()
         self._deferred.pop(0).callback(None)
-    
+
     def __str__(self):
         return json.dumps({'label' : self._name, 'linestyle' : self._style,
                            'color' : self._color, 'data' : self._data})
@@ -82,23 +82,23 @@ class TestBase(object):
 class RemoteTest(TestBase):
     def __init__(self, conn, name, style, color, testType, testName):
         super(RemoteTest, self).__init__(conn, name, style, color)
-        
+
         self._testType = testType
         self._testName = testName
-        
+
         self._srv = None
-    
+
     def _activate(self):
-        self._srv = self._conn.service('tester', 'Test/StringTest',
-                                       self._process)
-    
+        self._srv = self._conn.serviceClient('tester', 'Test/StringTest',
+                                             self._process)
+
     def _deactivate(self):
         self._srv = None
-    
+
     def _run(self):
         self._srv.call({'testType' : self._testType,
                         'testName' : self._testName})
-    
+
     def _process(self, msg):
         self._data.append(msg['times'])
         self._done()
@@ -107,21 +107,21 @@ class RemoteTest(TestBase):
 class LocalTest(TestBase):
     def __init__(self, conn, name, style, color, sizes):
         super(LocalTest, self).__init__(conn, name, style, color)
-        
+
         self._sizes = sizes
-    
+
     def _run(self):
         self._data.append([])
         self._req()
-    
+
     def _resp(self, resp):
         stop = time.time()
-        
+
         if self._str != resp['data']:
             delta = -1
         else:
-            delta = (stop-self._time)*1000
-        
+            delta = (stop - self._time) * 1000
+
         self._data[-1].append(delta)
         self._req()
 
@@ -129,25 +129,26 @@ class LocalTest(TestBase):
 class LocalServiceTest(LocalTest):
     def __init__(self, conn, name, style, color, sizes):
         super(LocalServiceTest, self).__init__(conn, name, style, color, sizes)
-        
+
         self._srv = None
-    
+
     def _activate(self):
-        self._srv = self._conn.service('test', 'Test/StringEcho', self._resp)
-    
+        self._srv = self._conn.serviceClient('test', 'Test/StringEcho',
+                                             self._resp)
+
     def _deactivate(self):
         self._srv = None
-    
+
     def _req(self):
         count = len(self._data[-1])
-        
+
         if count >= len(self._sizes):
             self._done()
             return
-        
+
         self._str = ''.join(random.choice(string.lowercase)
                             for _ in xrange(self._sizes[count]))
-        
+
         self._time = time.time()
         self._srv.call({'data' : self._str})
 
@@ -155,95 +156,95 @@ class LocalServiceTest(LocalTest):
 class LocalTopicTest(LocalTest):
     def __init__(self, conn, name, style, color, sizes):
         super(LocalTopicTest, self).__init__(conn, name, style, color, sizes)
-        
+
         self._pub = None
         self._sub = None
-    
+
     def _activate(self):
         self._pub = self._conn.publisher('testReqTopic',
                                          'std_msgs/String')
         self._sub = self._conn.subscriber('testRespTopic',
                                           'std_msgs/String', self._resp)
-    
+
     def _deactivate(self):
         self._pub = None
         self._sub.unsubscribe()
         self._sub = None
-    
+
     def _req(self):
         count = len(self._data[-1])
-        
+
         if len(self._data[-1]) >= len(self._sizes):
             self._done()
             return
-        
+
         if self._sizes[count] > 10:
             sample = ''.join(random.choice(string.lowercase)
                              for _ in xrange(10))
-            rep = int(self._sizes[count]/10)
-            tail = 'A'*(self._sizes[count] % 10)
-            
-            self._str = sample*rep+tail
+            rep = int(self._sizes[count] / 10)
+            tail = 'A' * (self._sizes[count] % 10)
+
+            self._str = sample * rep + tail
         else:
             self._str = ''.join(random.choice(string.lowercase)
                                 for _ in xrange(self._sizes[count]))
-        
+
         self._time = time.time()
         self._pub.publish({'data' : self._str})
-    
+
 
 class Benchmark(object):
     TYPES = [('service', 'red'), ('topic', 'blue')]
     REMOTE_TESTS = [('N-to-N', ':', 'stringEcho'), ('C-to-C-1', '--', 'local'),
                     ('C-to-C-2', '-', 'remote')]
     LOCAL_TEST = ('R-to-C', '-.', '')
-    
+
     def __init__(self, runs, conn, robot, reactor):
         self._runs = runs
         self._sizes = [3, 10, 50, 100, 500, 1000, 5000,
                        10000, 50000, 100000, 500000]
         #self._sizes = [3, 10, 100, 1000, 10000, 100000, 500000,
         #               1000000, 5000000, 10000000]
-        
+
         self._conn = conn
         self._robot = robot
         self._reactor = reactor
-        
+
         self._cTag = ['m1c1', 'm2c1', 'm1c2', 'm2c2']
-        
+
         self._tests = []
-        
+
         for testType, color in self.TYPES:
             for name, style, testName in self.REMOTE_TESTS:
                 self._tests.append(RemoteTest(conn,
                     '{0} {1}'.format(testType, name), style, color, testType,
                     testName))
-            
+
             if testType == 'service':
                 cls = LocalServiceTest
             elif testType == 'topic':
                 cls = LocalTopicTest
             else:
                 raise ValueError('Test type is invalid.')
-            
+
             self._tests.append(
                 cls(conn, '{0} {1}'.format(testType, self.LOCAL_TEST[0]),
                     self.LOCAL_TEST[1], color, self._sizes))
-    
+
     def run(self, _):
         print('Setup environments...')
-        
+
         ### Add containers
         for cTag in self._cTag:
             self._conn.createContainer(cTag)
-        
+
         ### Add the nodes
         for cTag in self._cTag:
             self._conn.addNode(cTag, 'strEcho', 'Test', 'stringEcho.py')
-        
+
         self._conn.addNode(self._cTag[0], 'strTester', 'Test',
                            'stringTester.py')
-        
+
         ### Add interfaces and connections
         # Connections Robot - StringTester
         tag = 'tester'
@@ -254,7 +255,7 @@ class Benchmark(object):
                                 cls)
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._robot, tag))
-        
+
         tag = 'testerData'
         cls = 'Test/StringData'
         self._conn.addInterface(self._cTag[0], tag, 'PublisherInterface',
@@ -263,7 +264,7 @@ class Benchmark(object):
                                 cls)
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._robot, tag))
-        
+
         # Connections StringTester - StringEcho (service)
         tag = 'testLocal'
         cls = 'Test/StringEcho'
@@ -273,7 +274,7 @@ class Benchmark(object):
                                 cls, 'stringEchoService')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[2], tag))
-        
+
         tag = 'testRemote'
         cls = 'Test/StringEcho'
         self._conn.addInterface(self._cTag[0], tag, 'ServiceProviderInterface',
@@ -282,28 +283,28 @@ class Benchmark(object):
                                 cls, 'stringEchoService')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[1], tag))
-        
+
         # Connections StringTester - StringEcho (topic)
         tag = 'testResp'
         cls = 'std_msgs/String'
         self._conn.addInterface(self._cTag[0], tag, 'PublisherInterface',
                                 cls, 'stringEchoResp')
-        
+
         self._conn.addInterface(self._cTag[1], tag, 'SubscriberInterface',
                                 cls, 'stringEchoResp')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[1], tag))
-        
+
         self._conn.addInterface(self._cTag[2], tag, 'SubscriberInterface',
                                 cls, 'stringEchoResp')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[2], tag))
-        
+
         self._conn.addInterface(self._robot, tag, 'SubscriberConverter',
                                 cls)
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._robot, tag))
-        
+
         tag = 'testLocalReq'
         cls = 'std_msgs/String'
         self._conn.addInterface(self._cTag[0], tag, 'SubscriberInterface',
@@ -312,7 +313,7 @@ class Benchmark(object):
                                 cls, 'stringEchoReq')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[2], tag))
-        
+
         tag = 'testRemoteReq'
         cls = 'std_msgs/String'
         self._conn.addInterface(self._cTag[0], tag, 'SubscriberInterface',
@@ -321,7 +322,7 @@ class Benchmark(object):
                                 cls, 'stringEchoReq')
         self._conn.addConnection('{0}/{1}'.format(self._cTag[0], tag),
                                  '{0}/{1}'.format(self._cTag[1], tag))
-        
+
         # Connections Robot - StringEcho (service)
         tag = 'test'
         cls = 'Test/StringEcho'
@@ -331,7 +332,7 @@ class Benchmark(object):
                                 cls)
         self._conn.addConnection('{0}/{1}'.format(self._cTag[3], tag),
                                  '{0}/{1}'.format(self._robot, tag))
-        
+
         # Connections Robot - StringEcho (topic)
         tag = 'testReqTopic'
         cls = 'std_msgs/String'
@@ -349,45 +350,45 @@ class Benchmark(object):
                                 cls)
         self._conn.addConnection('{0}/{1}'.format(self._cTag[3], tag),
                                  '{0}/{1}'.format(self._robot, tag))
-        
+
         self._reactor.callLater(20, self._load)
-    
+
     def _load(self):
         self._conn.publisher('testerData', 'Test/StringData').publish(
             {'size' : self._sizes})
-        
+
         if self._tests:
             self._reactor.callLater(2, self._run)
-    
+
     def _run(self):
         if not self._tests:
             return
-        
+
         i = 0
         d = Deferred()
-        
+
         while i < self._runs:
             for test in self._tests:
                 d.addCallback(test.run)
                 d = Deferred()
                 test.registerDeferred(d)
-            
+
             i += 1
-            
+
             if i >= self._runs:
                 d.addCallback(self._processData)
-        
+
         self._tests[0].run(None)
-    
+
     def _processData(self, _):
         with open('benchmark.data', 'w') as f:
             f.write(json.dumps(self._sizes))
             f.write('\n')
             f.write('\n'.join(str(test) for test in self._tests))
-        
+
         for cTag in self._cTag:
             self._conn.destroyContainer(cTag)
-        
+
         self._reactor.callLater(2, self._reactor.stop)
         print('\ndone')
 
@@ -405,25 +406,25 @@ def _get_argparse():
                         type=str)
 
     return parser
-    
+
 
 def main(reactor, passes, ip):
     user = 'testUser'
     robot = 'testRobot'
     connection = Connection(user, robot, user, reactor)
     benchmark = Benchmark(passes, connection, robot, reactor)
-    
+
     print('Connect...')
     deferred = Deferred()
     deferred.addCallback(benchmark.run)
     connection.connect('http://{0}:9000/'.format(ip), deferred)
-    
+
     reactor.run()
 
 
 if __name__ == '__main__':
     from twisted.internet import reactor
-    
+
     args = _get_argparse().parse_args()
-    
+
     main(reactor, args.passes, args.ipMaster)
--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#     
+#
 #     plot.py
-#     
+#
 #     This file is part of the RoboEarth Cloud Engine framework.
-#     
+#
 #     This file was originally created for RoboEearth
 #     http://www.roboearth.org/
-#     
+#
 #     The research leading to these results has received funding from
 #     the European Union Seventh Framework Programme FP7/2007-2013 under
 #     grant agreement no248942 RoboEarth.
-#     
+#
 #     Copyright 2012 RoboEarth
-#     
+#
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
 #     You may obtain a copy of the License at
-#     
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#     
+#
 #     Unless required by applicable law or agreed to in writing, software
 #     distributed under the License is distributed on an "AS IS" BASIS,
 #     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
-#     
-#     \author/s: Dominique Hunziker 
-#     
-#     
+#
+#     \author/s: Dominique Hunziker
+#
+#
 
 import numpy
 import pylab
@@ -40,26 +40,26 @@ def main(fileNames, ylog, remote, plotStyle):
         pylab.subplot(111, xscale='log', yscale='log')
     else:
         pylab.subplot(111, xscale='log')
-    
+
     for fileName in fileNames:
         with open(fileName, 'r') as f:
             size = numpy.array(json.loads(f.readline())[1:])
             benchmark = map(json.loads, f.readlines())
-        
+
         for test in benchmark:
             if not remote and test['label'].endswith('R-to-C'):
                 continue
-            
+
             rawData = test.pop('data')
             data = [d for d in rawData if -1.0 not in d]
             length = len(data)
-            
+
             if length != len(rawData):
                 print('Invalid measurement encountered in '
                       '{0}.'.format(test['label']))
-            
+
             data = numpy.array(data)
-            
+
             if length > 1:
                 mean = numpy.mean(data, 0)[1:]
                 var = numpy.var(data, 0)[1:]
@@ -71,7 +71,7 @@ def main(fileNames, ylog, remote, plotStyle):
             else:
                 print('No valid measurements for {0}.'.format(test['label']))
                 continue
-            
+
             if plotStyle == 'minmax':
                 pylab.errorbar(size, mean, numpy.array([minVal, maxVal]),
                                **test)
@@ -79,10 +79,10 @@ def main(fileNames, ylog, remote, plotStyle):
                 pylab.errorbar(size, mean, var, **test)
             else:
                 pylab.errorbar(size, mean, **test)
-    
+
     if 'paper.data' in fileNames:
         pylab.figtext(0.8, 0.8, 'green: paper measurements', ha='right')
-    
+
     pylab.xlabel('number of characters')
     pylab.ylabel('time [ms]')
     pylab.legend(loc='upper left')
@@ -91,11 +91,11 @@ def main(fileNames, ylog, remote, plotStyle):
 
 def _get_argparse():
     from argparse import ArgumentParser
-    
+
     parser = ArgumentParser(prog='benchmark',
                             description='Run communication benchmark using a '
                                         'string message for RCE.')
-    
+
     parser.add_argument('--ylog', help='Flag to activate log scale for y axis.',
                         action='store_true')
     parser.add_argument('--show', help='Flag to activate the display the R-to-C'
@@ -105,16 +105,16 @@ def _get_argparse():
                         type=str, choices=('minmax', 'variance'))
     parser.add_argument('--paper', help='Flag to add measurement from paper for'
                                         ' comparison.', action='store_true')
-    
+
     return parser
 
 
 if __name__ == '__main__':
     args = _get_argparse().parse_args()
-    
+
     files = ['benchmark.data']
-    
+
     if args.paper:
         files.append('paper.data')
-    
+
     main(files, args.ylog, args.show, args.errorbar)
