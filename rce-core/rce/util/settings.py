6e0d10eec7555c3f6f70bed679cbea8f3460258b
Dominique Hunziker <dominique.hunziker@gmail.com>
2013-04-25 01:12:27 +0200

    revision of provision script

--- rce-core/rce/util/settings.py
+++ rce-core/rce/util/settings.py
@@ -138,8 +138,8 @@ class _Settings(object):
         self._gzip_lvl = None
         self._dev_mode = None
         self._pw_file = None
-        self._rosrel = None
-        self._baserel = None
+        self._ros_release = None
+        self._base_release = None
 
         # Network
         self._external_ip = None
@@ -182,18 +182,18 @@ class _Settings(object):
         """ Path to the credentials database.
         """
         return self._pw_file
-    
+
     @property
-    def rosrel(self):
-        """ Path to the credentials database.
+    def ros_release(self):
+        """ ROS release used inside the container.
         """
-        return self._rosrel
-    
+        return self._ros_release
+
     @property
-    def baserel(self):
-        """ Path to the credentials database.
+    def base_release(self):
+        """ Ubuntu release used inside the container..
         """
-        return self._baserel
+        return self._base_release
 
     @property
     def external_IP(self):
@@ -323,8 +323,8 @@ class _Settings(object):
         settings._gzip_lvl = parser.getint('global', 'gzip_lvl')
         settings._dev_mode = parser.getboolean('global', 'dev_mode')
         settings._pw_file = parser.get('global', 'password_file')
-        settings._rosrel = parser.get('global', 'rosrel')
-        settings._baserel = parser.get('global', 'baserel')
+        settings._ros_release = parser.get('global', 'ros_release')
+        settings._base_release = parser.get('global', 'base_release')
 
         # Network
         settings._external_ip = parser.getIP('network', 'external_if')
